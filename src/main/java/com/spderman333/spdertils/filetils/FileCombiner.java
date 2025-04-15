c65ff97352f79b74f050b78a7a35392993afa284
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2025-04-15 10:20:34 -0700

    FileCombiner class functional.

--- src/main/java/com/spderman333/spdertils/filetils/FileCombiner.java
+++ src/main/java/com/spderman333/spdertils/filetils/FileCombiner.java
@@ -1,8 +1,51 @@
 package com.spderman333.spdertils.filetils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
-interface FileCombiner {
-    public File combine(String sourceDir, String destination, boolean recursivelyScour) throws IOException;
+public class FileCombiner {
+
+    public static File combine(String sourceDir, String destination) throws IOException {
+
+        File combinedFile = new File(destination);
+        combinedFile.createNewFile();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(combinedFile));
+
+        ArrayList<File> fileredSource = scourDirectory(new File(sourceDir));
+
+        File[] asArrayFile = fileredSource.toArray(new File[fileredSource.size()]);
+
+        for (File file : asArrayFile) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.append(line);
+                bufferedWriter.newLine();
+            }
+
+            bufferedReader.close();
+        }
+
+        bufferedWriter.close();
+        return combinedFile;
+    };
+
+    private static ArrayList<File> scourDirectory(File directoryToScan) {
+        File[] listedFiles = directoryToScan.listFiles();
+
+        ArrayList<File> outputList = new ArrayList<>();
+
+        if (listedFiles != null) {
+            for (File file : listedFiles) {
+                if (file.isFile()) {
+                    outputList.add(file);
+                } else {
+                    outputList.addAll(scourDirectory(file));
+                }
+            }
+        }
+
+        return outputList;
+    }
 }
