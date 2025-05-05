1ce7a47689493a1aba34ab40b326b144a28a8faa
spderman3333 <118777573+spderman3333@users.noreply.github.com>
2025-05-04 19:30:47 -0700

    We making a math library now.

--- src/main/java/com/spderman333/spdertils/filetils/FileCombiner.java
+++
@@ -1,8 +0,0 @@
-package com.spderman333.spdertils.filetils;
-
-import java.io.File;
-import java.io.IOException;
-
-interface FileCombiner {
-    public File combine(String sourceDir, String destination, boolean recursivelyScour) throws IOException;
-}
